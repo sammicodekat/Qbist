54e23dd9d4a04701c5bcb7c78c11365ca3ce55dd
mavarius <metalwhirlwind@yahoo.com>
2016-10-13 13:25:28 -0700

    Added more pieces and glasses

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -21,10 +21,11 @@ export default class Canvas extends Component {
       const {faceAttributes, faceLandmarks, faceRectangle} = imgData
       let landmarks = faceLandmarks
       const {age,facialHair, gender, glasses, headPose, smile} = faceAttributes
-      let {eyeLeftOuter, eyeLeftTop, eyeLeftBottom, eyeLeftInner} = landmarks
-      let {eyeRightOuter, eyeRightTop, eyeRightBottom, eyeRightInner} = landmarks
-      let {noseRootLeft} = landmarks
-      let {pupilRight, pupilLeft, underLipBottom, underLipTop, upperLipBottom, upperLipTop} = landmarks
+      let {pupilLeft, eyeLeftOuter, eyeLeftTop, eyeLeftBottom, eyeLeftInner} = landmarks
+      let {pupilRight, eyeRightOuter, eyeRightTop, eyeRightBottom, eyeRightInner} = landmarks
+      let {eyebrowLeftOuter, eyebrowLeftInner, eyebrowRightOuter, eyebrowRightInner} = landmarks
+      let {noseRootLeft, noseLeftAlarOutTip} = landmarks
+      let {underLipBottom, underLipTop, upperLipBottom, upperLipTop, mouthLeft, mouthRight} = landmarks
       let {top, left, width, height} = faceRectangle
 
       const relW = faceRectangle.width / 600
@@ -37,14 +38,17 @@ export default class Canvas extends Component {
 
       const ctx = canvas.getContext('2d')
       ctx.clearRect(0, 0, 600, 600)
-      setTimeout(this._createNew(ctx, 'head', 1, 50, 50), 6000)
-      setTimeout(this._createNew(ctx, 'eye', 7, eyeLeftTop.x, eyeLeftTop.y), 6000)
-      setTimeout(this._createNew(ctx, 'eye', 7, eyeRightTop.x, eyeRightTop.y), 6000)
-      setTimeout(this._createNew(ctx, 'nose', 4, noseRootLeft.x, noseRootLeft.y), 6000)
-      setTimeout(this._createNew(ctx, 'pupil', 7, pupilLeft.x, pupilLeft.y), 6000)
-      setTimeout(this._createNew(ctx, 'pupil', 7, pupilRight.x, pupilRight.y), 6000)
-      setTimeout(this._createNew(ctx, 'topLip', 4, upperLipTop.x, upperLipTop.y), 6000)
-      setTimeout(this._createNew(ctx, 'bottomLip', 4, underLipBottom.x, underLipBottom.y), 6000)
+      this._createNew(ctx, 'head', 5, 0, 0)
+      this._createNew(ctx, 'eye', 7, eyeLeftOuter.x, eyeLeftTop.y)
+      this._createNew(ctx, 'eye', 7, eyeRightInner.x, eyeRightTop.y)
+      this._createNew(ctx, 'nose', 4, noseLeftAlarOutTip.x, noseRootLeft.y)
+      this._createNew(ctx, 'pupil', 7, pupilLeft.x, pupilLeft.y)
+      this._createNew(ctx, 'pupil', 7, pupilRight.x, pupilRight.y)
+      this._createNew(ctx, 'topLip', 4, mouthLeft.x, upperLipTop.y)
+      this._createNew(ctx, 'bottomLip', 4, mouthLeft.x, underLipBottom.y)
+      this._createNew(ctx, 'brow', 10, eyebrowLeftOuter.x, eyebrowLeftOuter.y)
+      this._createNew(ctx, 'brow', 10, eyebrowRightInner.x, eyebrowRightInner.y)
+      if (glasses !== 'NoGlasses') this._createNew(ctx, 'ReadingGlasses', 3, noseRootLeft.x - noseRootLeft.x, noseRootLeft.y - 50)
     }
     // ctx.fillRect(300, 300, 500, 500)
   }
@@ -55,8 +59,9 @@ export default class Canvas extends Component {
     item = new Image()
     item.src = source
     if (item === 'head') {
+      ctx.save()
       ctx.translate(300, 300)
-      ctx.rotate(headPose.roll*Math.PI/180)
+      ctx.rotate(this.props.faceAttributes.headPose.roll * Math.PI / 180)
       item.onload = () => ctx.drawImage(item, -xp, -yp)
       ctx.restore()
     } else {
