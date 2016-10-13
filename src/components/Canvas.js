f1b91ce6b50a57a19d107db51b0fc86413e4c6fb
mavarius <metalwhirlwind@yahoo.com>
2016-10-13 01:45:50 -0700

    added more stuff

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,62 +1,67 @@
 import React, { Component } from 'react'
 
 export default class Canvas extends Component {
-  constructor(){
+  constructor () {
     super()
-    this.updateCanvas=this.updateCanvas.bind(this);
+    this.updateCanvas = this.updateCanvas.bind(this)
   }
 
   componentDidMount () {
     this.updateCanvas()
   }
 
-  componentDidUpdate (){
+  componentDidUpdate () {
     this.updateCanvas()
   }
 
   updateCanvas () {
-
     const {canvas} = this.refs
     const {imgData} = this.props
-    if(imgData.faceAttributes){
-    const {faceAttributes,faceLandmarks,faceRectangle} = imgData
-    let landmarks = faceLandmarks
-    const {age,facialHair,gender,glasses,headPose,smile} = faceAttributes
-    let {eyeLeftOuter,eyeLeftTop,eyeLeftBottom,eyeLeftInner} = landmarks
-    let {eyeRightOuter,eyeRightTop,eyeRightBottom,eyeRightInner} = landmarks
-    let {noseRootLeft} = landmarks
-    let {pupilRight,pupilLeft,underLipBottom,underLipTop,upperLipBottom,upperLipTop} = landmarks
-    let {top,left,width,height} = faceRectangle;
+    if (imgData.faceAttributes) {
+      const {faceAttributes, faceLandmarks, faceRectangle} = imgData
+      let landmarks = faceLandmarks
+      const {age,facialHair, gender, glasses, headPose, smile} = faceAttributes
+      let {eyeLeftOuter, eyeLeftTop, eyeLeftBottom, eyeLeftInner} = landmarks
+      let {eyeRightOuter, eyeRightTop, eyeRightBottom, eyeRightInner} = landmarks
+      let {noseRootLeft} = landmarks
+      let {pupilRight, pupilLeft, underLipBottom, underLipTop, upperLipBottom, upperLipTop} = landmarks
+      let {top, left, width, height} = faceRectangle
 
-    const relW = faceRectangle.width/600;
-    const relH = faceRectangle.height/600;
+      const relW = faceRectangle.width / 600
+      const relH = faceRectangle.height / 600
 
-    for ( var coord in landmarks){
-      landmarks[coord].x = (landmarks[coord].x - faceRectangle.left)/relW
-      landmarks[coord].y = (landmarks[coord].y - faceRectangle.top)/relH
+      for (var coord in landmarks) {
+        landmarks[coord].x = (landmarks[coord].x - faceRectangle.left) / relW
+        landmarks[coord].y = (landmarks[coord].y - faceRectangle.top) / relH
+      }
+
+      const ctx = canvas.getContext('2d')
+      ctx.clearRect(0, 0, 600, 600)
+      setTimeout(this._createNew(ctx, 'head', 1, 50, 50), 6000)
+      setTimeout(this._createNew(ctx, 'eye', 7, eyeLeftTop.x, eyeLeftTop.y), 6000)
+      setTimeout(this._createNew(ctx, 'eye', 7, eyeRightTop.x, eyeRightTop.y), 6000)
+      setTimeout(this._createNew(ctx, 'nose', 4, noseRootLeft.x, noseRootLeft.y), 6000)
+      setTimeout(this._createNew(ctx, 'pupil', 7, pupilLeft.x, pupilLeft.y), 6000)
+      setTimeout(this._createNew(ctx, 'pupil', 7, pupilRight.x, pupilRight.y), 6000)
+      setTimeout(this._createNew(ctx, 'topLip', 4, upperLipTop.x, upperLipTop.y), 6000)
+      setTimeout(this._createNew(ctx, 'bottomLip', 4, underLipBottom.x, underLipBottom.y), 6000)
     }
-
-    console.log("imgData",imgData);
-    const ctx = canvas.getContext('2d')
-    ctx.clearRect(0,0,600,600);
-    this._createNew("eye01",eyeLeftOuter.x,eyeLeftOuter.y,ctx)
-    this._createNew("eye01",eyeRightInner.x,eyeRightInner.y,ctx)
-    this._createNew("nose01",noseRootLeft.x-50,noseRootLeft.y+50,ctx)
-    this._createNew('pupil01',pupilLeft.x,pupilLeft.y+25,ctx)
-    this._createNew('pupil01',pupilRight.x,pupilRight.y+25,ctx)
-    this._createNew('topLip01',upperLipTop.x,upperLipTop.y,ctx)
-    this._createNew('bottomLip01',underLipBottom.x,underLipBottom.y,ctx)
-
-    // this._createNew("wideEye",300,300,ctx)
-  }
     // ctx.fillRect(300, 300, 500, 500)
   }
 
-  _createNew(item,xp,yp,ctx){
-    let source = `./images/${item}.png`
+  _createNew (ctx, item, itemNum, xp, yp) {
+    let num = (Math.floor(Math.random() * itemNum) + 1)
+    let source = `./images/${item}${num}.png`
     item = new Image()
-    item.src = source;
-    item.onload = () => ctx.drawImage(item, xp, yp)
+    item.src = source
+    if (item === 'head') {
+      ctx.translate(300, 300)
+      ctx.rotate(headPose.roll*Math.PI/180)
+      item.onload = () => ctx.drawImage(item, -xp, -yp)
+      ctx.restore()
+    } else {
+      item.onload = () => ctx.drawImage(item, xp, yp)
+    }
   }
 
   render () {
